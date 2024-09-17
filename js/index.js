e2c217f7a3823e5529c244bee324af8ffac10609
Andrea-LaVentura75 <laventuraandrea40@gmail.com>
2024-09-17 19:31:45 +0200

    second commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// let nascondiPrimaSezione = document.getElementById("nascondi1");
+
+// let sezioneSaldi = document.getElementById("saldi");
+
+// nascondiPrimaSezione.addEventListener("click", function () {
+//   sezioneSaldi.classList.toggle("invisible");
+// });
